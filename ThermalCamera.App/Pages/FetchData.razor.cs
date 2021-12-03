0da2c8cb923594ac2d4042715624e7a6452d8f55
Matt Jeanes <matthew.jeanes@myairops.com>
2021-12-03 02:51:12 +0000

    Add rudimentary display of thermal output

--- ThermalCamera.App/Pages/FetchData.razor.cs
+++ ThermalCamera.App/Pages/FetchData.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Excubo.Blazor.Canvas;
+using Microsoft.AspNetCore.Components;
 using ThermalCamera.App.Data;
 using ThermalCamera.App.Data.Interfaces;
 
@@ -9,12 +10,18 @@ public partial class FetchData
     [Inject]
     private UsbConnectionService UsbConnectionService { get; set; } = default!;
 
+    private const int SENSOR_WIDTH = 32;
+    private const int SENSOR_HEIGHT = 24;
+    private const int CANVAS_SCALE = 16;
+
 
     private List<UsbConnectionData>? _usbConnectionData = new List<UsbConnectionData>();
     private IDeviceStream? _deviceStream;
     private string _streamData = string.Empty;
+    private decimal[]? _temperatureData;
     private string _statusMessage = string.Empty;
     private string _errorMessage = string.Empty;
+    private Canvas _canvas = default!;
 
     protected override async Task OnInitializedAsync()
     {
@@ -44,7 +51,7 @@ public partial class FetchData
 
     private async void HandleOutputEvent(object? sender, OutputEventArgs args)
     {
-        await InvokeAsync(() =>
+        await InvokeAsync(async () =>
         {
             var splitIndex = args.Output.IndexOf(':');
             if (splitIndex == -1)
@@ -65,7 +72,7 @@ public partial class FetchData
             switch (command)
             {
                 case "data":
-                    ProcessData(data);
+                    await ProcessDataAsync(data);
                     break;
                 case "error":
                     ProcessError(data);
@@ -81,23 +88,46 @@ public partial class FetchData
         });
     }
 
-    private void ProcessData(string data)
+    private async Task ProcessDataAsync(string data)
     {
         try
         {
             if (data.Contains("data:")) { return; } // sometimes the stream contains randomly 'data:' on first connection, so we ignore this
             var array = data.TrimEnd(',').Split(',').Select(x => decimal.Parse(x)).ToArray();
-            if (array.Length != 768)
+            if (array.Length != (SENSOR_WIDTH * SENSOR_HEIGHT))
             {
                 _errorMessage = $"Array length is different from expected, length: {array.Length}";
                 return;
             }
             _streamData = Timestamp(data);
+            _temperatureData = array;
+            await UpdateTemperatureDisplay();
         }
         catch (Exception e)
         {
             _errorMessage = Timestamp($"Failed to process data: {e}");
         }
+    }
+
+    private async Task UpdateTemperatureDisplay()
+    {
+        if (_temperatureData == null) { return; }
+        await using var ctx = await _canvas.GetContext2DAsync();
+        await using var batch = ctx.CreateBatch();
+        for (var i = 0; i < _temperatureData.Length; i++)
+        {
+            var column = i % SENSOR_WIDTH;
+            var row = (i - column) / SENSOR_WIDTH;
+            var tempX = (SENSOR_HEIGHT - row - 1) * SENSOR_WIDTH;
+            var temp = _temperatureData[tempX + column];
+            var tempRGB = Math.Clamp(temp * 8, 0, 255);
+            await batch.FillAndStrokeStyles.FillStyleAsync($"rgb({tempRGB},{tempRGB},{tempRGB})");
+            await batch.FillRectAsync(column * CANVAS_SCALE, row * CANVAS_SCALE, CANVAS_SCALE, CANVAS_SCALE);
+        }
+        var midTemp = _temperatureData[((SENSOR_HEIGHT/2) * SENSOR_WIDTH) + (SENSOR_WIDTH / 2)];
+        await batch.FontAsync("20px serif");
+        await batch.FillAndStrokeStyles.FillStyleAsync("#ff0000");
+        await batch.FillTextAsync(midTemp + "°C", (SENSOR_WIDTH * CANVAS_SCALE) / 2, (SENSOR_HEIGHT * CANVAS_SCALE) / 2);
     }
 
     private void ProcessError(string data)
