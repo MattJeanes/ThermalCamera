fee22cc1e258a64df9eb4fc43e7ce4275e7310af
Matt Jeanes <mattjeanes23@gmail.com>
2023-05-01 22:42:10 +0100

    re-add attributes for usb serial connection

--- ThermalCamera.App/Platforms/Android/MainActivity.cs
+++ ThermalCamera.App/Platforms/Android/MainActivity.cs
@@ -1,9 +1,13 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
+using Android.Hardware.Usb;
+
+[assembly: UsesFeature("android.hardware.usb.host")]
 
 namespace ThermalCamera.App;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[IntentFilter(new[] { UsbManager.ActionUsbDeviceAttached })]
+[MetaData(UsbManager.ActionUsbDeviceAttached, Resource = "@xml/device_filter")]
 public class MainActivity : MauiAppCompatActivity
 {
 }
